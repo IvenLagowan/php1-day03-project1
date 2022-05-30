4b4d7be2c2690cad7a990bc0893b192b3a1edf3d
Iven DF <94290469+ivendf@users.noreply.github.com>
2022-05-30 03:41:23 -0700

    simpan pertama

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  $judul = 'SaCode - PHP BATCH 1 DAY 03';
+  $isi = 'Belajar PHP untuk Pemulai Di SaCode';
+
+ ?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $judul;?></title>
+  </head>
+  <body>
+    <h1><?php print $judul;?> </h1>
+    <p><?php print $isi;?></p>
+
+  </body>
+</html>
